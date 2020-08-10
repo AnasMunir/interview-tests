f61b63549aee6ecda5f4581ae4fa78c6cf46d8d0
AnasMunir <anas.munir@bramerz.pk>
2020-08-10 18:16:27 +0500

    made test2

---
+++ test2/locations.interface.js
@@ -0,0 +1,76 @@
+locations = {
+	"location_id": "3DFEAEW2-KJA23-AS22",
+	"Name": "Earth",
+	"Address": "Milky Way Galaxy, Alpha Quadrant, near Mars",
+	"Phone": "111-111-111",
+	"AddressContinue": null,
+	"Zip": "56000",
+	"Locality": "Testing location name",
+	"Country": "Pakistan",
+	"Province": "Punjab",
+	"City": "Lahore",
+	"Latitude": "22.22222",
+	"Longitude": "11.111111",
+	"enable_store_timings": true,
+	"branch_timings": {
+		"Monday": {
+			"timingsEnabled": true,
+			"timings": [
+				{ "timeOpen": "00:01", "timeClose": "01:00" },
+				{ "timeOpen": "12:00", "timeClose": "00:00" }
+			]
+		},
+		"Tuesday": {
+			"timingsEnabled": true,
+			"timings": [
+				{ "timeOpen": "00:01", "timeClose": "01:00" },
+				{ "timeOpen": "12:00", "timeClose": "00:00" }
+			]
+		},
+		"Wednesday": {
+			"timingsEnabled": true,
+			"timings": [
+				{ "timeOpen": "00:01", "timeClose": "01:00" },
+				{ "timeOpen": "12:00", "timeClose": "00:00" }
+			]
+		},
+		"Thursday": {
+			"timingsEnabled": true,
+			"timings": [
+				{ "timeOpen": "00:01", "timeClose": "01:00" },
+				{ "timeOpen": "12:00", "timeClose": "00:00" }
+			]
+		},
+		"Friday": {
+			"timingsEnabled": true,
+			"timings": [
+				{ "timeOpen": "00:01", "timeClose": "01:00" },
+				{ "timeOpen": "12:00", "timeClose": "00:00" }
+			]
+		},
+		"Saturday": {
+			"timingsEnabled": true,
+			"timings": [
+				{ "timeOpen": "00:01", "timeClose": "01:00" },
+				{ "timeOpen": "12:00", "timeClose": "00:00" }
+			]
+		},
+		"Sunday": {
+			"timingsEnabled": true,
+			"timings": [
+				{ "timeOpen": "00:01", "timeClose": "01:00" },
+				{ "timeOpen": "12:00", "timeClose": "03:00" }
+			]
+		}
+	},
+	"status": true,
+	"specific_timings": true,
+	"delivery_areas": [
+		"Earth Quadrant a",
+		"Earth Quadrant b",
+		"Earth Quadrant c",
+		"Earth Quadrant d",
+		"Earth Quadrant e"
+	]
+};
+module.exports = locations;
---
+++ test2/test2.js
@@ -0,0 +1,27 @@
+'use strict';
+const location = require('./locations.interface');
+
+class Test {
+	constructor() {
+		displayLocations(); // You can comment this if you want
+	}
+}
+
+const displayLocations = () => {
+	console.log('Location', location);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Do not edit below this line
+const test = new Test();
