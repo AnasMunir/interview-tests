4de7ef2a9bc7715bc442560773d6fafb67d7324b
AnasMunir <anas.munir@bramerz.pk>
2020-08-11 12:04:19 +0500

    made part 2 of test 2

--- test2/README.md
+++ test2/README.md
@@ -1,4 +1,4 @@
-## Test-2
+## Test-2 Part One
 ### If you've completed test-1, then get ready for another challenge
 You can see there's a location object being printed, you must do the following
 1. Make a copy of the locations object and in it, convert the values of all the `timingsEnabled` object to false.
@@ -8,3 +8,5 @@ Note:
 - Do Not use `for-loops` nor `forEach`.
 - It's encouraged that you divide your code into **pure** functions.
 - The way you format your overall code will be closely analyzed.
+
+## Test-2 Part Two, instructions are in the file partTwo.js
---
+++ test2/partTwo.js
@@ -0,0 +1,28 @@
+const ourAsyncFunc = () => {
+	console.log("1. We are Borg");
+	setTimeout(function () {
+		console.log("2. You will be assimilated!");
+	}, 2000);
+	console.log("3. Resistance is futile!");
+}
+
+
+ourAsyncFunc();
+
+
+
+
+// Do not edit below this line
+/* 
+	* When you run this code, you will see the following printed:
+		1. We are Borg
+		3. Resistance is futile!
+		2. You will be assimilated!
+
+	* Keeping the sequence of the console.log in the same order, you must print the following in order
+		1. We are Borg
+		2. You will be assimilated!
+		3. Resistance is futile!
+
+	Note: JavaScript has multiple ways to make async code synchronous, you must use any of the following ways available and make the above code synchronous.
+ */
